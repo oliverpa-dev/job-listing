03a225e94c42b0bb784f7f58c30e34df6ea300ee
oliverpa-dev <oliver.paszkiwicz@interia.pl>
2023-11-12 21:13:42 +0100

    Add: function for handling filters

--- src/components/list-item/list-item.tsx
+++ src/components/list-item/list-item.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./main.scss";
 import { ItemFilters } from "./item-filters/item-filters";
 import { ItemSubLabels } from "./item-sub-labels/item-sub-labels";
@@ -24,33 +24,48 @@ interface Item {
 }
 
 export const ListItem: React.FC<ListItemProps> = ({ data }) => {
+  const [state, setState] = useState<string[]>([]);
+
+  const handleOnClick = (clickedFilter: string) => {
+    if (state.length === 0) {
+      setState([clickedFilter]);
+    } else {
+      setState((prevState) => {
+        return [...prevState, clickedFilter];
+      });
+    }
+  };
+
+  console.log(state);
+
   return (
     <div className="list-item__container">
-      {data.map((el) => (
+      {data.map((item) => (
         <div className="list-item__wrapper">
           <div className="list-item__copy">
             <div className="list-item__copy__labels">
               <div className="list-item__copy__label">
-                {el.company}
-                {el.featured}
-                {el.new}
+                {item.company}
+                {item.featured}
+                {item.new}
               </div>
             </div>
-            <div className="list-item__copy__title">{el.position}</div>
+            <div className="list-item__copy__title">{item.position}</div>
             <div className="list-item__copy__sub-labels">
               <ItemSubLabels
-                postedAt={el.postedAt}
-                contract={el.contract}
-                location={el.location}
+                postedAt={item.postedAt}
+                contract={item.contract}
+                location={item.location}
               />
             </div>
           </div>
           <div className="list-item__filters">
             <ItemFilters
-              tools={el.tools}
-              languages={el.languages}
-              role={el.role}
-              level={el.level}
+              tools={item.tools}
+              languages={item.languages}
+              role={item.role}
+              level={item.level}
+              onSelect={handleOnClick}
             />
           </div>
         </div>
