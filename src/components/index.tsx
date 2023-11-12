3107505128fb1e6e796c32bfef0abe621423f29c
oliverpa-dev <oliver.paszkiwicz@interia.pl>
2023-11-12 18:29:19 +0100

    Add: pass data as a prop

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { ListItem } from "./list-item/list-item";
+import "./main.scss";
+import data from "../data.json";
 
 export const ListContainer = () => {
-  return <></>;
+  return (
+    <div className="list-container">
+      <ListItem data={data} />
+    </div>
+  );
 };
